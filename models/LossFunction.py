db4ebfb96cdd77a050cc54d42a112c4f11e6ed16
Robin <robinttt333@gmail.com>
2020-02-06 18:38:33 +0530

    Tweak

--- models/LossFunction.py
+++ models/LossFunction.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+from globalVariables import FRAME_COUNT
 class NLLSequenceLoss(nn.Module):
 
     def __init__(self):
@@ -9,7 +9,7 @@ class NLLSequenceLoss(nn.Module):
         loss = 0.0
         transposed = input.transpose(0,1).contigous()
 
-        for i in range(0, 29):
+        for i in range(0, FRAME_COUNT):
             loss += self.criterion(transposed[i], target)
         
         return loss
