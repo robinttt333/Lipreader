f7a979ef7d6de8b3c6e0b499cb0666cbf4d04328
Robin <robinttt333@gmail.com>
2020-02-09 22:36:16 +0530

    Cuda check added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from globalVariables import (IMAGE_CHANNELS,CONV3dOUTPUT_CHANNELS,CONV3D_PADDING,
-CONV3d_KERNEL,CONV3d_STRIDE,IMAGE_TRANSFORMS,BATCH_SIZE,SHUFFLE,FRONTEND_POOL_KERNEL,
-FRONTEND_POOL_STRIDE,FRONTEND_POOL_PADDING,RESNET_MODEL,PRE_TRAIN_RESNET,
-ENCODER_REPRESENTATION_SIZE,LSTM_HIDDEN_SIZE,FRAME_COUNT,LSTM_LAYERS,NUM_CLASSES,BACKEND_TYPE,
-BN_SIZE,CONV1_KERNEL,CONV1_STRIDE,CONV2_KERNEL,CONV2_STRIDE,MAX_POOL1_KERNEL,MAX_POOL1_STRIDE,
-EPOCHS,COMPLETED_EPOCHS,LEARNING_RATE,MOMENTUM)
+from globalVariables import (IMAGE_CHANNELS, CONV3dOUTPUT_CHANNELS, CONV3D_PADDING,
+                             CONV3d_KERNEL, CONV3d_STRIDE, IMAGE_TRANSFORMS, BATCH_SIZE, SHUFFLE, FRONTEND_POOL_KERNEL,
+                             FRONTEND_POOL_STRIDE, FRONTEND_POOL_PADDING, RESNET_MODEL, PRE_TRAIN_RESNET,
+                             ENCODER_REPRESENTATION_SIZE, LSTM_HIDDEN_SIZE, FRAME_COUNT, LSTM_LAYERS, NUM_CLASSES, BACKEND_TYPE,
+                             BN_SIZE, CONV1_KERNEL, CONV1_STRIDE, CONV2_KERNEL, CONV2_STRIDE, MAX_POOL1_KERNEL, MAX_POOL1_STRIDE,
+                             EPOCHS, COMPLETED_EPOCHS, LEARNING_RATE, MOMENTUM)
 
 from train import Trainer
 import os
@@ -13,57 +13,57 @@ if __name__ == "__main__":
     '''
     path = "."
     paramsEncoder = {
-        "inputChannels" : IMAGE_CHANNELS,
-        "outputChannels" : CONV3dOUTPUT_CHANNELS,
-        "stride" : CONV3d_STRIDE,
-        "padding" : CONV3D_PADDING,
-        "kernel" : CONV3d_KERNEL,
-        "poolKernel" : FRONTEND_POOL_KERNEL,
-        "poolStride" : FRONTEND_POOL_STRIDE,
-        "poolPadding" : FRONTEND_POOL_PADDING,
-        "resnetModel" : RESNET_MODEL,
-        "preTrain" : PRE_TRAIN_RESNET,
-        "frames" : FRAME_COUNT,
-        "backend_type" : BACKEND_TYPE
-        }
+        "inputChannels": IMAGE_CHANNELS,
+        "outputChannels": CONV3dOUTPUT_CHANNELS,
+        "stride": CONV3d_STRIDE,
+        "padding": CONV3D_PADDING,
+        "kernel": CONV3d_KERNEL,
+        "poolKernel": FRONTEND_POOL_KERNEL,
+        "poolStride": FRONTEND_POOL_STRIDE,
+        "poolPadding": FRONTEND_POOL_PADDING,
+        "resnetModel": RESNET_MODEL,
+        "preTrain": PRE_TRAIN_RESNET,
+        "frames": FRAME_COUNT,
+        "backend_type": BACKEND_TYPE
+    }
     if BACKEND_TYPE == "temporal CNN":
         paramsDecoder = {
-            "conv1_kernel" : CONV1_KERNEL,
-            "conv1_stride" : CONV1_STRIDE,
-            "conv2_kernel" : CONV2_KERNEL,
-            "conv2_stride" : CONV2_STRIDE,
-            "max_pool1_kernel" : MAX_POOL1_KERNEL,
-            "max_pool1_stride" : MAX_POOL1_STRIDE,
-            "bn_size" : BN_SIZE,
-            "inputFeatures" : ENCODER_REPRESENTATION_SIZE,
-            "hiddenDimensions" :  LSTM_HIDDEN_SIZE,
-            "frames" : FRAME_COUNT,
-            "lstmLayers" : LSTM_LAYERS,
-            "classes" : NUM_CLASSES,
-            "backend_type" : BACKEND_TYPE   
-    }
-    else :
+            "conv1_kernel": CONV1_KERNEL,
+            "conv1_stride": CONV1_STRIDE,
+            "conv2_kernel": CONV2_KERNEL,
+            "conv2_stride": CONV2_STRIDE,
+            "max_pool1_kernel": MAX_POOL1_KERNEL,
+            "max_pool1_stride": MAX_POOL1_STRIDE,
+            "bn_size": BN_SIZE,
+            "inputFeatures": ENCODER_REPRESENTATION_SIZE,
+            "hiddenDimensions":  LSTM_HIDDEN_SIZE,
+            "frames": FRAME_COUNT,
+            "lstmLayers": LSTM_LAYERS,
+            "classes": NUM_CLASSES,
+            "backend_type": BACKEND_TYPE
+        }
+    else:
         paramsDecoder = {
 
-        "inputFeatures" : ENCODER_REPRESENTATION_SIZE,
-        "hiddenDimensions" :  LSTM_HIDDEN_SIZE,
-        "frames" : FRAME_COUNT,
-        "lstmLayers" : LSTM_LAYERS,
-        "classes" : NUM_CLASSES,
-        "backend_type" : BACKEND_TYPE
-    }
+            "inputFeatures": ENCODER_REPRESENTATION_SIZE,
+            "hiddenDimensions":  LSTM_HIDDEN_SIZE,
+            "frames": FRAME_COUNT,
+            "lstmLayers": LSTM_LAYERS,
+            "classes": NUM_CLASSES,
+            "backend_type": BACKEND_TYPE
+        }
     dataParams = {
-        "path" : path,
-        "batch_size" : BATCH_SIZE,
-        "shuffle" : SHUFFLE,
-        "transforms" : IMAGE_TRANSFORMS,
-        "mode" : "train"
+        "path": path,
+        "batch_size": BATCH_SIZE,
+        "shuffle": SHUFFLE,
+        "transforms": IMAGE_TRANSFORMS,
+        "mode": "train"
     }
     hyperParams = {
-        "learningRate" : LEARNING_RATE,
-        "momentum" : MOMENTUM
+        "learningRate": LEARNING_RATE,
+        "momentum": MOMENTUM
     }
-    trainer = Trainer(paramsEncoder,paramsDecoder,hyperParams,dataParams)
-    
-    for epoch in range(COMPLETED_EPOCHS,EPOCHS):
-        trainer.train() 
+    trainer = Trainer(paramsEncoder, paramsDecoder, hyperParams, dataParams)
+
+    for epoch in range(COMPLETED_EPOCHS, EPOCHS):
+        trainer.train()
--- train.py
+++ train.py
@@ -1,24 +1,40 @@
 from data.dataset import LRWDataset
+import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from models.lipReader import Lipreader
 from torch import optim
+
+
 class Trainer():
-    def __init__(self,paramsEncoder,paramsDecoder,hyperParams,dataParams):
-        self.lipreader = Lipreader(paramsEncoder,paramsDecoder)
-        self.dataset = LRWDataset(dataParams["path"],dataParams["mode"],dataParams["transforms"])
-        self.dataLoader =  DataLoader(self.dataset,batch_size = dataParams["batch_size"],
-        shuffle = dataParams["shuffle"])
+    def __init__(self, paramsEncoder, paramsDecoder, hyperParams, dataParams):
+        # Check if gpu is available
+        self.device = "gpu:0" if torch.cuda.is_available() else "cpu"
+        self.lipreader = Lipreader(
+            paramsEncoder, paramsDecoder)
+
+        if torch.cuda.device_count() > 1:
+            self.lipreader = nn.DataParallel(self.lipreader)
+        self.lipreader = self.lipreader.to(self.device)
+        print(self.lipreader)
+        # print(next(self.lipreader.parameters()).is_cuda)
+        self.dataset = LRWDataset(
+            dataParams["path"], dataParams["mode"], dataParams["transforms"])
+        self.dataLoader = DataLoader(self.dataset, batch_size=dataParams["batch_size"],
+                                     shuffle=dataParams["shuffle"])
         self.learningRate = hyperParams["learningRate"]
         self.momentum = hyperParams["momentum"]
 
     def train(self):
-        optimizer = optim.SGD(self.lipreader.parameters(),lr = self.learningRate,
-                    momentum = self.momentum)
+        optimizer = optim.SGD(self.lipreader.parameters(), lr=self.learningRate,
+                              momentum=self.momentum)
 
-        for _,batch in enumerate(self.dataLoader): 
-            optimizer.zero_grad()   
-            input,target = batch
+        for _, batch in enumerate(self.dataLoader):
+            optimizer.zero_grad()
+            input, target = batch
+            input = input.to(self.device)
+            target = target.to(self.device)
             output = self.lipreader(input)
-            loss = self.lipreader.loss(output,target)
+            loss = self.lipreader.loss(output, target)
             loss.backward()
             optimizer.step()
